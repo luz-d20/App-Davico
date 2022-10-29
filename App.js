d081d9893738ffaa69811f81d25c44c9c89fcdfe
Luz Davico <mldavico@gmail.com>
2022-10-29 18:34:13 -0300

    Added to-do list mockup

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <TextInput style={styles.item} placeholder="Agregar un item" />
+        <Button title="Agregar" />
+      </View>
+      <View style={styles.itemContainer}>
+        <Text style={styles.listItem}>Item 1</Text>
+        <Text style={styles.listItem}>Item 2</Text>
+        <Text style={styles.listItem}>Item 3</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1e1e1e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    width: '80%',
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    margin: 10,
+  },
+  item: {
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+  },
+  listItem:{
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
+    padding: 10,
+  }
 });
